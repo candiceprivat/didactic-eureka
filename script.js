3980986dadc6fc3237cf0dcc39302564ee6fc520
tdeborde <33256624+tristandeborde@users.noreply.github.com>
2021-04-15 13:05:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ let trainButton;
 
 function modelReady() {
   console.log('Model is ready!!!');
-  classifier.load(['/model.json', '/model.weights.bin'], customModelReady);
+  classifier.load(['/didactic-eureka/model.json', '/didactic-eureka/model.weights.bin'], customModelReady);
 }
 
 function customModelReady() {
